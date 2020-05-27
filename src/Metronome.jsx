b04f4b599c55ae7e960d4ee3df692e97e79613c7
Frank Rauen <home@Franks-MacBook-Air.local>
2020-05-26 19:07:09 -0600

    adds sound capability startstop and handlebpmchange functions

--- src/Metronome.jsx
+++ src/Metronome.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import styles from './Metronome.module.css';
 
+import click1 from './click1.wav';
+import click2 from './click2.wav';
+
+
 class Metronome extends Component {
     constructor(props){
         super(props);
@@ -11,13 +15,59 @@ class Metronome extends Component {
             bpm: 100,
             beatsPerMeasure: 4
         }
+        this.click1 = new Audio(click1);
+        this.click2 = new Audio(click2);
+        
     }
+
     handleBpmChange = e => {
         const bpm = e.target.value;
-        this.setState({ bpm })
-    
+        if (this.state.playing) {
+            clearInterval(this.timer);
+            this.timer = setInterval(this.playClick, (40 / bpm) * 1000)
+            this.setState({
+                count: 0,
+                bpm
+            });
+        } else {
+            this.setState({ bpm });
+        }
+        
+    };
 
-    }
+    startStop = () => {
+        if(this.state.playing) {
+            clearInterval(this.timer);
+            this.setState({
+                playing: false
+            });
+        } else {
+            this.timer = setInterval(
+                this.playClick,
+                (40 / this.state.bpm) * 1000
+            );
+            this.setState({
+                count: 0,
+                playing: true
+            },
+            this.playClick
+            );
+        }
+    };
+
+    playClick = () => {
+        const { count, beatsPerMeasure} = this.state;
+
+        if (count % beatsPerMeasure === 0) {
+            this.click2.play();
+        } else {
+            this.click1.play();
+        }
+        this.setState(state => ({
+            count: (state.count + 1) % state.beatsPerMeasure
+        }));
+    };      
+
     render() {
         const { playing, bpm } = this.state;
     
@@ -33,7 +83,9 @@ class Metronome extends Component {
                 value={bpm}
                 onChange={this.handleBpmChange}></input>
             </div>
-        <button>{playing ? 'Stop' : 'Start'}</button>
+        <button onClick={this.startStop}>
+            {playing ? 'Stop' : 'Start'}
+        </button>
         </div>)
     }
 }
